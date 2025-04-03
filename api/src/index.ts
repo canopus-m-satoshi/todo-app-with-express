d079ef40d61198f31c73d8234539dfae845a8191
Satoshi Matsushima <work.satoshi1993@gmail.com>
2025-04-03 23:30:16 +0900

    feat: ポートの設定

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World')
+import express, { Express } from 'express'
+
+const app: Express = express()
+const PORT = 8080
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
