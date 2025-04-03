bbe297562dad58d20767ab23a969580df129428c
Satoshi Matsushima <work.satoshi1993@gmail.com>
2025-04-04 00:09:31 +0900

    feat: allTodos apiの作成

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,11 @@
-import express, { Express } from 'express'
+import express, { Express, Request, Response } from 'express'
 
 const app: Express = express()
 const PORT = 8080
+
+app.get('/allTodos', (req: Request, res: Response) => {
+  return res.send('Todos')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
