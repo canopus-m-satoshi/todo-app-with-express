774e6463546f0443b516625f688e82b207e5ed70
Satoshi Matsushima <work.satoshi1993@gmail.com>
2025-04-05 00:55:22 +0900

    feat: Edit APIの実装

--- api/src/index.ts
+++ api/src/index.ts
@@ -35,6 +35,32 @@ app.post('/createTodo', async (req: Request, res: Response) => {
   }
 })
 
+app.put('/editTodo/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const { title, isCompleted } = req.body
+
+    const editTodo = await prisma.todo.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        title,
+        isCompleted,
+      },
+    })
+
+    return res.json(editTodo)
+  } catch (e) {
+    console.log(e)
+
+    return res.status(500).json({
+      message: 'Error editing todo',
+      error: e,
+    })
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
