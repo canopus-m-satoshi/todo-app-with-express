51e189cf85dfa63389814e4fd76425ab1fe59b78
Satoshi Matsushima <work.satoshi1993@gmail.com>
2025-04-06 01:15:10 +0900

    feat: バックエンド側にcorsミドルウェアを追加

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from 'express'
 import { PrismaClient } from '../generated/prisma'
+import cors from 'cors'
 
 const app: Express = express()
+app.use(cors())
 app.use(express.json())
 
 const PORT = 8080
