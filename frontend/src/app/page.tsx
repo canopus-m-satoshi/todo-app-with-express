2a9df3b7593544bd56352a032048f02454e4365e
Satoshi Matsushima <work.satoshi1993@gmail.com>
2025-04-08 00:00:51 +0900

    feat: タスク追加機能の実装

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,16 +3,49 @@
 import useSWR from 'swr'
 import Todo from './components/Todo'
 import { TodoType } from './types'
+import { useRef } from 'react'
+
+const postEndpoint = 'http://localhost:8080/createTodo'
+const getEndpoint = 'http://localhost:8080/allTodos'
 
 async function fetcher(key: string): Promise<TodoType[]> {
   return await fetch(key).then((res) => res.json())
 }
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR<TodoType[]>(
-    'http://localhost:8080/allTodos',
-    fetcher,
-  )
+  const {
+    data,
+    error,
+    isLoading,
+    mutate: mutateTodos,
+  } = useSWR<TodoType[]>(getEndpoint, fetcher)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const createTodo = async (data: Omit<TodoType, 'id'>) => {
+    const response = await fetch(postEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+
+    return response.json()
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const inputValue = inputRef.current?.value
+
+    if (!inputValue) return
+
+    const newTodo = await createTodo({
+      title: inputValue,
+      isCompleted: false,
+    })
+
+    mutateTodos([...(data || []), newTodo])
+  }
 
   if (isLoading) return <div>Loading...</div>
 
@@ -25,7 +58,9 @@ export default function Home() {
           To-Do List
         </h1>
       </div>
-      <form className="w-full max-w-sm mx-auto px-4 py-2">
+      <form
+        className="w-full max-w-sm mx-auto px-4 py-2"
+        onSubmit={handleSubmit}>
         <div className="flex items-center border-b-2 border-teal-500 py-2">
           <input
             className="appearance-none bg-transparent
@@ -33,6 +68,7 @@ export default function Home() {
       focus:outline-none"
             type="text"
             placeholder="Add a task"
+            ref={inputRef}
           />
           <button
             className="duration-150 flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
