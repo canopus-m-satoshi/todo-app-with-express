79849e3d6a3b5bdceca902020a0eeb55ef0cfb92
Satoshi Matsushima <work.satoshi1993@gmail.com>
2025-04-13 15:37:12 +0900

    chore: .envファイルの用意

--- frontend/src/app/constants/index.ts
+++ frontend/src/app/constants/index.ts
@@ -1,6 +1,6 @@
-const BASE_URL = 'http://localhost:8080'
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-export const GET_ENDPOINT = `${BASE_URL}/getAllTodos`
-export const POST_ENDPOINT = `${BASE_URL}/createTodo`
-export const EDIT_ENDPOINT = `${BASE_URL}/editTodo`
-export const DELETE_ENDPOINT = `${BASE_URL}/deleteTodo`
+export const GET_ENDPOINT = `${API_URL}/getAllTodos`
+export const POST_ENDPOINT = `${API_URL}/createTodo`
+export const EDIT_ENDPOINT = `${API_URL}/editTodo`
+export const DELETE_ENDPOINT = `${API_URL}/deleteTodo`
